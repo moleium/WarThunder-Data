e4b71ca61c9572625f19b9506523cbf3e61e5648
Mole <93382765+MoleSchizo@users.noreply.github.com>
2023-10-29 18:06:29 +0300

    Create guidance_laser.c

---
+++ guidance_laser.c
@@ -0,0 +1,6 @@
+void __fastcall guidance_laser(__int64 a1, const char **a2, int a3, _QWORD *a4)
+{
+  add_server_param(a4, 0x20i64, "Guidance: Laser\n", 0i64, 0);
+  if ( a3 > 0 && !strcmp(*a2, "autopilot") )
+    homing_autopilot((a1 + 0x80), (a2 + 1), a3 - 1, a4);
+}
