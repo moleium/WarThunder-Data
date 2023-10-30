b470dfb24bcea3e2bb27f17191f7128f3c349b26
Mole <93382765+MoleSchizo@users.noreply.github.com>
2023-10-30 18:28:30 +0300

    Update has_baydoor.c

--- has_baydoor.c
+++ has_baydoor.c
@@ -1,10 +1,11 @@
 bool has_baydoor()
 {
-  bool result; // al
+  bool has_bay_door; // al
 
-  result = 0;
+  has_bay_door = 0;
   if ( LocalUnit && !*(LocalUnit + 0x10B0) && !*(qword_4ACEE78 + 0x450) )
     return *(*(LocalUnit + 0x9D0) + 0x568Fi64) != 0;
-  // Unit + 0x9D0  : Air Movement
-  return result;
+  // Unit + 0x9D0 Air Movement
+  // Unit + 0x10B0 Unit Type (Plane, Tank, etc.)
+  return has_bay_door;
 }
