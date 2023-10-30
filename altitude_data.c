6378bffc65dbdfbb7711527895a6419ee3ba1601
Mole <93382765+MoleSchizo@users.noreply.github.com>
2023-10-30 13:02:15 +0300

    Update altitude_data.c

--- altitude_data.c
+++ altitude_data.c
@@ -1,4 +1,4 @@
-char __fastcall process_altitude_data(__int64 data, float *min_altitude, float *max_altitude, float *step_altitude)
+char __fastcall process_altitude_data(__int64 http_req, float *min_altitude, float *max_altitude, float *step_altitude)
 {
   _BYTE *dataptr; // rdi
   signed __int64 data_len; // rdx
@@ -8,18 +8,18 @@ char __fastcall process_altitude_data(__int64 data, float *min_altitude, float *
   signed __int64 v13; // rdx
   char Buffer[72]; // [rsp+30h] [rbp-48h] BYREF
 
-  dataptr = *(data + 0x18);
+  dataptr = *(http_req + 0x18);
   if ( dataptr )
-    data_len = strlen(*(data + 0x18));
+    data_len = strlen(*(http_req + 0x18));
   else
     data_len = 0i64;
   if ( !parse_command_data(dataptr, data_len, "minAlt", Buffer, 0x20i64) || format_as(Buffer, "%f") != 1 )
     return 0;
-  v10 = *(data + 0x18);
-  v11 = v10 ? strlen(*(data + 0x18)) : 0i64;
+  v10 = *(http_req + 0x18);
+  v11 = v10 ? strlen(*(http_req + 0x18)) : 0i64;
   if ( !parse_command_data(v10, v11, "maxAlt", Buffer, 0x20i64) || format_as(Buffer, "%f") != 1 )
     return 0;
-  v12 = *(data + 0x18);
+  v12 = *(http_req + 0x18);
   v13 = v12 ? strlen(v12) : 0i64;
   if ( !parse_command_data(v12, v13, "stepAlt", Buffer, 0x20i64) || format_as(Buffer, "%f") != 1 )
     return 0;
