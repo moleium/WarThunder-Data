770603a7384978db82895fec78cb32b0ca59b46b
Mole <93382765+MoleSchizo@users.noreply.github.com>
2023-11-03 02:15:54 +0300

    Create quit_to_debriefing.c

---
+++ quit_to_debriefing.c
@@ -0,0 +1,17 @@
+__int64 quit_to_debriefing()
+{
+  __int64 result; // rax
+  int v1; // [rsp+20h] [rbp-18h] BYREF
+  const char *v2; // [rsp+28h] [rbp-10h]
+
+  v2 = "quit_to_debriefing";
+  v1 = 1;
+  dg_debug(3u, "%s", &v1, 1);
+  result = game;
+  if ( !*(game + 0x2C5) )
+  {
+    *(game + 0x2C0) = 0x3F800000;
+    *(result + 0x2C4) = 1;
+  }
+  return result;
+}
