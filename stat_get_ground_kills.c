1d93574f39e5407ea77d9ddcb1054a4edf4ed65d
Mole <93382765+MoleSchizo@users.noreply.github.com>
2023-11-03 03:32:09 +0300

    Create stat_get_ground_kills.c

---
+++ stat_get_ground_kills.c
@@ -0,0 +1,4 @@
+__int64 __fastcall stat_get_ground_kills(unsigned int a1, unsigned int a2, unsigned int a3)
+{
+  return *(*(game + 0x430) + 0x1F8i64 * a2 + 0xA8i64 * a1 + 0x18i64 * a3 + 0x5148);
+}
