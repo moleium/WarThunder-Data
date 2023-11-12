b80b1c4b9ebe24d00d6c940449c0610b5dfef688
Mole <93382765+MoleSchizo@users.noreply.github.com>
2023-11-12 16:27:53 +0300

    Create is_alive.c

---
+++ unit/is_alive.c
@@ -0,0 +1,7 @@
+bool __fastcall isAlive(__int64 Unit)
+{
+  return Unit
+      && (*(Unit + 0x10B0) || *(Unit + 0x10A8) <= 1u)
+      && ((*(Unit + 0x1058) & 0x20000) == 0 || (*(*(Unit + 0x50) + 0x10i64))(Unit + 0x50))
+      && *(Unit + 0x10A8) < 2u;
+}
