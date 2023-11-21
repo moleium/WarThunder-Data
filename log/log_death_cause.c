0c509fc6b310d9a1a56694648799e801eba451e5
Mole <93382765+MoleSchizo@users.noreply.github.com>
2023-11-21 22:33:43 +0300

    Update log_death_cause.c

--- log/log_death_cause.c
+++ log/log_death_cause.c
@@ -1,4 +1,4 @@
-__int64 __fastcall log_death_cause(__int64 a1, _QWORD *a2)
+double __fastcall log_death_cause(__int64 ballistics_AA0, _QWORD *a2)
 {
   __int64 v4; // rcx
   unsigned int v5; // eax
@@ -14,9 +14,9 @@ __int64 __fastcall log_death_cause(__int64 a1, _QWORD *a2)
   __int64 v15; // rbx
   __int64 v16; // rdx
   __int64 v17; // rdx
-  __int64 v18; // r12
-  __int64 v19; // r8
-  __int64 v20; // r9
+  __int64 v18; // r9
+  __int64 Unit; // r12
+  __int64 v20; // r8
   __int64 v21; // rbp
   __int64 v22; // rdi
   __int64 v23; // rbx
@@ -27,31 +27,31 @@ __int64 __fastcall log_death_cause(__int64 a1, _QWORD *a2)
   __int64 v28; // rdi
   char *v29; // rcx
   __int64 v30; // r8
-  __int64 v31; // r9
-  __int64 result; // rax
-  __int64 v33; // rax
-  __int64 v34; // rcx
-  float v35; // xmm0_4
-  float v36; // xmm1_4
-  int v37; // r15d
-  __int64 v38; // rcx
-  int v39; // ebp
-  int v40; // ecx
-  __int64 v41; // rcx
-  int v42; // eax
-  __int64 v43; // rcx
-  int v44; // eax
+  double result; // xmm0_8
+  __int64 v32; // rax
+  __int64 v33; // rcx
+  float v34; // xmm0_4
+  float v35; // xmm1_4
+  int v36; // r15d
+  __int64 v37; // rcx
+  int v38; // ebp
+  int v39; // ecx
+  __int64 v40; // rcx
+  int v41; // eax
+  __int64 v42; // rcx
+  int v43; // eax
   _QWORD *ThreadLocalStoragePointer; // rcx
-  __int64 v46; // rax
-  unsigned int v47; // eax
-  const char *v48; // rax
-  __int64 v49; // rax
-  int v50; // ecx
-  int v51; // esi
-  __int64 v52; // rax
-  char v53; // bl
-  unsigned int v54; // edi
-  _QWORD *v55; // rcx
+  __int64 v45; // rax
+  int v46; // eax
+  const char *v47; // rax
+  __int64 v48; // rax
+  int v49; // ecx
+  int v50; // esi
+  __int64 v51; // rax
+  char v52; // bl
+  int v53; // edi
+  _QWORD *v54; // rcx
+  __int64 v55; // r9
   CHAR *v56; // rdx
   __int64 v57; // r15
   __int64 v58; // rsi
@@ -63,7 +63,7 @@ __int64 __fastcall log_death_cause(__int64 a1, _QWORD *a2)
   char v64; // al
   char *v65; // rcx
   __int64 v66; // rax
-  __int64 v67; // rax
+  char *v67; // rax
   CHAR *v68; // r8
   __int64 v69; // rax
   CHAR *v70; // rdi
@@ -101,7 +101,7 @@ __int64 __fastcall log_death_cause(__int64 a1, _QWORD *a2)
   v4 = dword_4B19940;
   if ( !dword_4B19940 )
   {
-    v5 = sub_1623330(0, 0x87C, 0, "drawPatc", 0);
+    v5 = sub_1623330(0i64, 0x87C, 0, "drawPatc", 0);
     v4 = v5;
     dword_4B19940 = v5;
   }
@@ -140,20 +140,20 @@ __int64 __fastcall log_death_cause(__int64 a1, _QWORD *a2)
   sub_18A2920(*a2, 1i64, 0i64);
   if ( *(game + 0x420) )
   {
-    v18 = *&mUnit;
+    Unit = *&mUnit;
     if ( !*&mUnit
       || (*(*&mUnit + 0x1058i64) & 0x7000014) != 0
       || !(*(*(*&mUnit + 0x50i64) + 0x18i64))(*&mUnit + 0x50i64)
-      || *(a1 + 0x1C9D) )
+      || *(ballistics_AA0 + 0x1C9D) )
     {
       goto LABEL_11;
     }
-    if ( *(v18 + 0x10A8) <= 1u )
+    if ( *(Unit + 0x10A8) <= 1u )
     {
-      if ( *(v18 + 0x10A8) != 1 )
+      if ( *(Unit + 0x10A8) != 1 )
       {
 LABEL_11:
-        v21 = *(a1 + 0x648);
+        v21 = *(ballistics_AA0 + 0x648);
         if ( *(v21 + 0x2E0) <= 0.0 )
         {
           if ( *(v21 + 0x110) )
@@ -174,103 +174,103 @@ LABEL_11:
         }
         if ( qword_4B19AA0 && (byte_4B1993D & 1) != 0 )
         {
-          sub_B27BC0(qword_4B19AA0 + 0x5A8, "hint:died_from_ammo_explosion:hide", &qword_4B67850);
+          sub_B27BC0(qword_4B19AA0 + 0x5A8, "hint:died_from_ammo_explosion:hide", &qword_4B67850, v18);
           byte_4B1993D = 0;
         }
 LABEL_21:
-        if ( v18 && *(a1 + 0x1C9D) && !*(v18 + 0x10A8) )
-          *(a1 + 0x1C9D) = 0;
+        if ( Unit && *(ballistics_AA0 + 0x1C9D) && !*(Unit + 0x10A8) )
+          *(ballistics_AA0 + 0x1C9D) = 0;
         goto LABEL_25;
       }
-      if ( (*(*v18 + 0x68i64))(v18) > *(*(game + 0x498) + 0x1E5Ci64) )
+      if ( (*(*Unit + 0x68i64))(Unit) > *(*(game + 0x498) + 0x1E5Ci64) )
       {
-        sub_BA6750(*(a1 + 0x648));
-        *(a1 + 0x1C9D) = 1;
+        sub_BA6750(*(ballistics_AA0 + 0x648));
+        *(ballistics_AA0 + 0x1C9D) = 1;
         goto LABEL_25;
       }
     }
-    v37 = *(v18 + 0x844);
-    if ( !*(*(a1 + 0x648) + 0x110i64) )
+    v36 = *(Unit + 0x844);
+    if ( !*(*(ballistics_AA0 + 0x648) + 0x110i64) )
       goto LABEL_73;
-    if ( (v37 - 0x11) <= 1 )
+    if ( (v36 - 0x11) <= 1 )
     {
-      v38 = *(game + 0x430) + 0x4F8i64;
+      v37 = *(game + 0x430) + 0x4F8i64;
       LODWORD(v83) = 0;
-      v39 = sub_145A390(v38, "timesShownExplosionDeathHint", &v83);
-      v40 = *(*(game + 0x498) + 0x2180i64);
-      if ( v39 <= v40 )
+      v38 = sub_145A390(v37, "timesShownExplosionDeathHint", &v83);
+      v39 = *(*(game + 0x498) + 0x2180i64);
+      if ( v38 <= v39 )
       {
-        sub_145A750(*(game + 0x430) + 0x4F8i64, "timesShownExplosionDeathHint", ++v39);
-        v40 = *(*(game + 0x498) + 0x2180i64);
+        sub_145A750(*(game + 0x430) + 0x4F8i64, "timesShownExplosionDeathHint", ++v38);
+        v39 = *(*(game + 0x498) + 0x2180i64);
       }
-      if ( v39 <= v40 && qword_4B19AA0 && (byte_4B1993D & 1) == 0 )
+      if ( v38 <= v39 && qword_4B19AA0 && (byte_4B1993D & 1) == 0 )
       {
-        sub_B27BC0(qword_4B19AA0 + 0x5A8, "hint:died_from_ammo_explosion:show", &qword_4B67850);
+        sub_B27BC0(qword_4B19AA0 + 0x5A8, "hint:died_from_ammo_explosion:show", &qword_4B67850, v18);
         byte_4B1993D = 1;
       }
     }
-    v41 = *(a1 + 0x648);
-    v42 = *(v41 + 0x110);
-    v43 = *(v41 + 0x100);
-    v44 = v42 - 1;
-    while ( v44 >= 0 )
+    v40 = *(ballistics_AA0 + 0x648);
+    v41 = *(v40 + 0x110);
+    v42 = *(v40 + 0x100);
+    v43 = v41 - 1;
+    while ( v43 >= 0 )
     {
-      v17 = *(v43 + 0x30i64 * v44-- + 0x2C);
+      v17 = *(v42 + 0x30i64 * v43-- + 0x2C);
       if ( v17 >= 0 )
         goto LABEL_71;
     }
     v17 = 0xFFFFFFFFi64;
 LABEL_71:
-    if ( (v37 - 0xA) <= 2 && v37 != v17 )
+    if ( (v36 - 0xA) <= 2 && v36 != v17 )
     {
 LABEL_73:
       ThreadLocalStoragePointer = NtCurrentTeb()->ThreadLocalStoragePointer;
       if ( !*(ThreadLocalStoragePointer[TlsIndex] + 1i64) )
-        _dyn_tls_on_demand_init(ThreadLocalStoragePointer, v17, v19, v20);
-      v46 = *(*(NtCurrentTeb()->ThreadLocalStoragePointer + TlsIndex) + 0x160i64);
+        _dyn_tls_on_demand_init(ThreadLocalStoragePointer, v17, v20, v18);
+      v45 = *(*(NtCurrentTeb()->ThreadLocalStoragePointer + TlsIndex) + 0x160i64);
       v91 = 0i64;
-      v92 = v46;
+      v92 = v45;
       v93 = 0i64;
-      *(a1 + 0x468) = *(v18 + 0x840);
-      v47 = *(v18 + 0x844);
-      *(a1 + 0x46C) = v47;
-      *(a1 + 0x470) = *(v18 + 0x848);
-      *(a1 + 0x472) = *(v18 + 0x84A);
-      *(a1 + 0x473) = *(v18 + 0x84B);
-      if ( !v47 && *(v18 + 0x199C) == 0xFFFF )
+      *(ballistics_AA0 + 0x468) = *(Unit + 0x840);
+      v46 = *(Unit + 0x844);
+      *(ballistics_AA0 + 0x46C) = v46;
+      *(ballistics_AA0 + 0x470) = *(Unit + 0x848);
+      *(ballistics_AA0 + 0x472) = *(Unit + 0x84A);
+      *(ballistics_AA0 + 0x473) = *(Unit + 0x84B);
+      if ( !v46 && *(Unit + 0x199C) == 0xFFFF )
       {
-        v53 = 0;
+        v52 = 0;
         goto LABEL_162;
       }
-      if ( (v37 - 0xA) <= 2 )
+      if ( (v36 - 0xA) <= 2 )
       {
-        sub_BA6750(*(a1 + 0x648));
-        sub_BA66E0(*(a1 + 0x648));
-        v47 = *(a1 + 0x46C);
+        sub_BA6750(*(ballistics_AA0 + 0x648));
+        sub_BA66E0(*(ballistics_AA0 + 0x648));
+        v46 = *(ballistics_AA0 + 0x46C);
       }
-      if ( v47 - 1 <= 0x1F )
+      if ( (v46 - 1) <= 0x1F )
       {
-        v48 = sub_BC2D50(dword_44CB900[v47]);
-        sub_185590(&v91, v48);
-        if ( !*(v18 + 0x10B0) )
+        v47 = sub_BC2D50(dword_44CB900[v46]);
+        sub_185590(&v91, v47);
+        if ( !*(Unit + 0x10B0) )
         {
-          v49 = *(v18 + 0x9D0);
-          if ( v49 )
+          v48 = *(Unit + 0x9D0);
+          if ( v48 )
           {
-            v50 = *(a1 + 0x46C);
-            if ( v50 == 5 )
+            v49 = *(ballistics_AA0 + 0x46C);
+            if ( v49 == 5 )
             {
-              *&v84 = *(v49 + 0x4618);
+              *&v84 = *(v48 + 0x4618);
               LODWORD(v83) = 3;
               add_server_param(&v91, 0x100i64, " %.2f", &v83, 1);
             }
-            else if ( v50 == 4 )
+            else if ( v49 == 4 )
             {
-              v51 = ((*(qword_4AD8F98 + 0x28i64 * *(*(game + 0x430) + 0x90i64)) * *(v49 + 0x4614)) + *&dword_44801CC);
-              v52 = sub_BC2D50(*(qword_4AD8F98 + 0x28i64 * *(*(game + 0x430) + 0x90i64) + 0x20));
-              v84 = v51;
+              v50 = ((*(qword_4AD8F98 + 0x28i64 * *(*(game + 0x430) + 0x90i64)) * *(v48 + 0x4614)) + *&dword_44801CC);
+              v51 = sub_BC2D50(*(qword_4AD8F98 + 0x28i64 * *(*(game + 0x430) + 0x90i64) + 0x20));
+              v84 = v50;
               LODWORD(v83) = 2;
-              v86 = v52;
+              v86 = v51;
               LODWORD(v85) = 1;
               add_server_param(&v91, 0x100i64, " %d %s", &v83, 2);
             }
@@ -280,44 +280,44 @@ LABEL_73:
           LODWORD(v56) = v91;
         else
           v56 = &szFile;
-        sub_B9AFE0(0x10, v56, 0, 0, *(a1 + 0x46C), 0, 0xBF800000, 1);
+        sub_B9AFE0(0x10, v56, 0, 0, *(ballistics_AA0 + 0x46C), 0, 0xBF800000, 1);
       }
-      v57 = *(v18 + 0x1940);
-      v53 = 1;
-      if ( !v57 || v57 == v18 )
+      v57 = *(Unit + 0x1940);
+      v52 = 1;
+      if ( !v57 || v57 == Unit )
       {
 LABEL_162:
         v17 = v91;
         if ( v91 )
           (*(*v92 + 0x40i64))(v92);
-        if ( v53 )
+        if ( v52 )
           goto LABEL_21;
         goto LABEL_25;
       }
       v83 = 0i64;
       v84 = off_4B678C8;
       v85 = 0i64;
-      v58 = sub_1206DE0(*(v18 + 0x1980));
+      v58 = sub_1206DE0(*(Unit + 0x1980));
       if ( !v58 )
         goto LABEL_141;
       v90 = 0i64;
-      v59 = *(v18 + 0x880);
+      v59 = *(Unit + 0x880);
       v60 = &szFile;
       if ( !v59 )
         v59 = &szFile;
       if ( sub_181B660(v59, 0i64) )
       {
         v61 = &szFile;
-        if ( *(v18 + 0x880) )
-          v61 = *(v18 + 0x880);
+        if ( *(Unit + 0x880) )
+          v61 = *(Unit + 0x880);
         v62 = sub_5400D0(v61);
         sub_277B70(&v90, v62);
-        v53 = 1;
+        v52 = 1;
       }
       else
       {
         sub_393750(&v99, &qword_49E7BED, off_4B678C8);
-        v63 = *(v18 + 0x880);
+        v63 = *(Unit + 0x880);
         if ( !v63 )
           v63 = &szFile;
         sub_196C10(&v99, &v94, v63);
@@ -329,7 +329,7 @@ LABEL_162:
         if ( v89 )
           v60 = v87;
         v64 = sub_181B660(v60, 0i64);
-        v53 = 1;
+        v52 = 1;
         if ( v64 )
         {
           if ( v89 )
@@ -387,8 +387,8 @@ LABEL_161:
           goto LABEL_162;
         }
       }
-      v73 = *(v18 + 0x10B0);
-      if ( v73 > 0xF || (v74 = 0x897E, v75 = _bittest(&v74, v73), v76 = v18, !v75) )
+      v73 = *(Unit + 0x10B0);
+      if ( v73 > 0xF || (v74 = 0x897E, v75 = _bittest(&v74, v73), v76 = Unit, !v75) )
         v76 = 0i64;
       v77 = "HUD_YOUR_KILLER";
       if ( !v58 )
@@ -427,7 +427,7 @@ LABEL_160:
       goto LABEL_39;
   }
 LABEL_25:
-  v26 = *(a1 + 0x454);
+  v26 = *(ballistics_AA0 + 0x454);
   if ( qword_4B83668 <= v26 )
   {
     v29 = 0i64;
@@ -454,40 +454,40 @@ LABEL_25:
   {
     goto LABEL_39;
   }
-  v33 = game;
+  v32 = game;
   if ( *(game + 0x6C4) >= 0.0 || *(qword_4B198C0 + 0x2B) && (dword_4A6C5A0 & 0x2F) == 0 )
     goto LABEL_39;
   v17 = NtCurrentTeb()->ThreadLocalStoragePointer;
-  v34 = *(v17 + 8i64 * TlsIndex);
-  if ( !*(v34 + 1) )
+  v33 = *(v17 + 8i64 * TlsIndex);
+  if ( !*(v33 + 1) )
   {
-    _dyn_tls_on_demand_init(v34, v17, v30, v31);
-    v33 = game;
+    _dyn_tls_on_demand_init(v33, v17, v30, v18);
+    v32 = game;
   }
-  if ( *(v33 + 0x254) )
+  if ( *(v32 + 0x254) )
   {
     if ( LocalPlayer )
-      v36 = *(LocalPlayer + 0x674);
+      v35 = *(LocalPlayer + 0x674);
     else
-      v36 = 0.0;
-    v35 = *(v33 + 0x240);
-    v54 = 0;
-    if ( (v35 - v36) > 0 )
-      v54 = (v35 - v36);
-    if ( dword_4A70BB8 != v54 )
+      v35 = 0.0;
+    v34 = *(v32 + 0x240);
+    v53 = 0;
+    if ( (v34 - v35) > 0 )
+      v53 = (v34 - v35);
+    if ( dword_4A70BB8 != v53 )
     {
-      dword_4A70BB8 = v54;
+      dword_4A70BB8 = v53;
       byte_4B1993E = 0;
     }
-    v55 = NtCurrentTeb()->ThreadLocalStoragePointer;
-    if ( !*(v55[TlsIndex] + 1i64) )
-      _dyn_tls_on_demand_init(v55, v17, v30, v31);
-    sub_14584E0(&v83, *(*(NtCurrentTeb()->ThreadLocalStoragePointer + TlsIndex) + 0x160i64));
+    v54 = NtCurrentTeb()->ThreadLocalStoragePointer;
+    if ( !*(v54[TlsIndex] + 1i64) )
+      _dyn_tls_on_demand_init(v54, v17, v30, v18);
+    sub_14584E0(&v83);
     if ( dword_4B1C508 <= 1 && !qword_4B507E8 )
-      assign_string_int(&v83, "timer", v54);
+      assign_string_int(&v83, "timer", v53);
     if ( qword_4B19AA0 && (byte_4B1993E & 1) == 0 )
     {
-      sub_B27BC0(qword_4B19AA0 + 0x5A8, "hint:press_a_continue:show", &v83);
+      sub_B27BC0(qword_4B19AA0 + 0x5A8, "hint:press_a_continue:show", &v83, v55);
       byte_4B1993E = 1;
     }
     sub_1458FF0(&v83);
@@ -497,14 +497,13 @@ LABEL_25:
 LABEL_39:
     if ( qword_4B19AA0 && (byte_4B1993E & 1) != 0 )
     {
-      sub_B27BC0(qword_4B19AA0 + 0x5A8, "hint:press_a_continue:hide", &qword_4B67850);
+      sub_B27BC0(qword_4B19AA0 + 0x5A8, "hint:press_a_continue:hide", &qword_4B67850, v18);
       byte_4B1993E = 0;
     }
   }
   LOBYTE(v17) = 1;
   sub_18A2CE0(*a2, v17);
-  sub_18A2920(*a2, 0i64, 0i64);
-  result = *a2;
+  result = sub_18A2920(*a2, 0i64, 0i64);
   *(*a2 + 0x2E0i64) = 0;
   *(a2 + 0x39) = 0;
   if ( v97 )
