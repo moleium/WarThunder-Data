afe1cc841a60f0c186c9dae75082452f66712612
moleium <molenoch@protonmail.com>
2024-10-30 19:15:52 +0300

    add: vac load subroutine and refactor the init

--- anti-cheat/vikingac/vikingac_init.c
+++ anti-cheat/vikingac/vikingac_init.c
@@ -1,53 +1,85 @@
-__int64 __fastcall vikingac::init(__int64 a1, const char *vikinac_path_)
+__int64 __fastcall vikingac::init(__int64 path_prefix, const char *override_dll_name)
 {
-  vikinac_path = "Client_64.dll";
-  if ( vikinac_path_ )
-    vikinac_path = vikinac_path_;
-  vikingac_module_ = LoadLibrary_unk(v3);
+  const char *dll_name;
+  const CHAR *final_path;
+  HMODULE vikingac_module_;
+  FARPROC vac_load_address_;
+  __int64 (*unload_func_address)(void);
+  unsigned int result;
+  DWORD error_code;
+  CHAR *dll_path;
+  DWORD error_code_;
+  CHAR *dll_path_;
+  CHAR *combined_path;
+  __int64 allocator;
+  int path_valid;
+  int log_param_1;
+  CHAR *log_path;
+  int log_param_2;
+  CHAR *error_message;
+
+  /*
+  result is enum class InitResult
+  enum class InitResult {
+    Success = 0,
+    DllLoadError = 2,
+    ExportError = 3
+  };
+  */
+
+  dll_name = "Client_64.dll";
+  if ( override_dll_name )
+    dll_name = override_dll_name;
+  concatenate_strings(path_prefix, &combined_path, dll_name);
+  if ( path_valid )
+    final_path = combined_path;
+  else
+    final_path = &szFile;
+  vikingac_module_ = LoadLibrary_unk(final_path);
   vikingac_module = vikingac_module_;
   if ( vikingac_module_ )
   {
-    ProcAddress = GetProcAddress(vikingac_module_, "Load");
-    vac_load_address = ProcAddress;
+    vac_load_address_ = GetProcAddress(vikingac_module_, "Load");
+    vac_load_address = vac_load_address_;
     unload_func_address = GetProcAddress(vikingac_module, "Unload");
     vac_unload_address = unload_func_address;
-    if ( !ProcAddress || (v7 = 0, !unload_func_address) )
+    if ( !vac_load_address_ || (result = 0, !unload_func_address) )
     {
-      LastError = GetLastError();
-      FormatMessageA(0x12FFu, 0i64, LastError, 0x409u, byte_4C66160, 0x400u, 0i64);
-      if ( v15 )
-        v9 = v13;
+      error_code = GetLastError();
+      FormatMessageA(0x12FFu, 0i64, error_code, 0x409u, lib_load_err, 0x400u, 0i64);
+      if ( path_valid )
+        dll_path = combined_path;
       else
-        v9 = &szFile;
-      v17 = v9;
-      v16 = 1;
-      v19 = byte_4C66160;
-      v18 = 1;
-      logmessage_fmt(1u, "[AC] Failed to load symbols from dll '%s' with '%s'", &v16, 2);
+        dll_path = &szFile;
+      log_path = dll_path;
+      log_param_1 = 1;
+      error_message = lib_load_err;
+      log_param_2 = 1;
+      logmessage_fmt(1u, "[AC] Failed to load symbols from dll '%s' with '%s'", &log_param_1, 2);
       if ( vikingac_module )
         FreeLibrary(vikingac_module);
       vikingac_module = 0i64;
       vac_load_address = 0i64;
       vac_unload_address = 0i64;
-      v7 = 3;
+      result = 3;
     }
   }
   else
   {
-    lastError = GetLastError();
-    FormatMessageA(0x12FFu, 0i64, lastError, 0x409u, byte_4C66160, 0x400u, 0i64);
-    if ( v15 )
-      v11 = v13;
+    error_code_ = GetLastError();
+    FormatMessageA(0x12FFu, 0i64, error_code_, 0x409u, lib_load_err, 0x400u, 0i64);
+    if ( path_valid )
+      dll_path_ = combined_path;
     else
-      v11 = &szFile;
-    v17 = v11;
-    v16 = 1;
-    v19 = byte_4C66160;
-    v18 = 1;
-    v7 = 2;
-    logmessage_fmt(1u, "[AC] Failed to load dll '%s' with '%s'", &v16, 2);
+      dll_path_ = &szFile;
+    log_path = dll_path_;
+    log_param_1 = 1;
+    error_message = lib_load_err;
+    log_param_2 = 1;
+    result = 2;
+    logmessage_fmt(1u, "[AC] Failed to load dll '%s' with '%s'", &log_param_1, 2);
   }
-  if ( v13 )
-    (*(*v14 + 0x40i64))(v14);
-  return v7;
+  if ( combined_path )
+    (*(*allocator + 0x40i64))(allocator);
+  return result;
 }
---
+++ anti-cheat/vikingac/vikingac_load.c
@@ -0,0 +1,44 @@
+void vikingac::load()
+{
+  char *message; // rdx
+  __int128 dagmem_alloc_; // [rsp+20h] [rbp-28h] BYREF
+  void (__fastcall *vac_on_debug_state_callback_)(int, __int64, __int64, __int64); // [rsp+30h] [rbp-18h]
+  __int64 v3[2]; // [rsp+38h] [rbp-10h] BYREF
+
+  if ( DWORD2(vac_unk_var) )
+  {
+    if ( vikingac_module )
+    {
+      *&dagmem_alloc_ = dagmem_alloc;
+      *(&dagmem_alloc_ + 1) = 0i64;
+      vac_on_debug_state_callback_ = vac_on_debug_state_callback;
+      load_successful = vac_load_address(vac::vac_session_id, &dagmem_alloc_);
+      if ( load_successful )
+      {
+        vac_dagmem = *(&dagmem_alloc_ + 1);
+LABEL_11:
+        DWORD2(vac_unk_var) = 0;
+        return;
+      }
+      message = "[AC] Client load call unsuccessful";
+    }
+    else
+    {
+      message = "[AC] Can't load client, no DLL.";
+    }
+    logmessage_fmt(2u, message, 0i64, 0);
+    if ( byte_4B841C8 == 1 )
+    {
+      dagmem_alloc_ = 0i64;
+      v3[0] = &dagmem_alloc_;
+      v3[1] = 1i64;
+      statsd::counter("ac.load_fail", v3, 1i64);
+    }
+    else
+    {
+      statsd::counter_x("ac.load_fail", 1i64);
+    }
+    logmessage_fmt(1u, "[AC] Failed to load, when server demands it", 0i64, 0);
+    goto LABEL_11;
+  }
+}
