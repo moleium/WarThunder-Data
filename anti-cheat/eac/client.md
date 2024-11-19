86cf427ae90bd5caf5219c999770168324c3459c
moleium <molenoch@protonmail.com>
2024-11-19 22:54:27 +0300

    refactor: eac::update, add check_integrity_violation to docs

--- anti-cheat/eac/client.md
+++ anti-cheat/eac/client.md
@@ -5,4 +5,5 @@
 ## Offsets
 - **0x10** - `connection_reset(eac::client)`
 - **0x20** - `send_net_message(*eac, eac::client, int8)`
+- **0x28** - `check_integrity_violation(*eac, callback_fn, 0, var)`
 - **0x68** - `set_auth_ticket()`
--- anti-cheat/eac/eac_update.c
+++ anti-cheat/eac/eac_update.c
@@ -10,26 +10,27 @@ __int64 eac::update()
   __int64 (__fastcall *v7)(void *, size_t, __int64, __int64); // [rsp+48h] [rbp-40h]
   int unk_var[4]; // [rsp+50h] [rbp-38h] BYREF
   LRESULT (__fastcall *manager_invoker_2)(int, WPARAM, LPARAM); // [rsp+60h] [rbp-28h]
-  void (__fastcall *debug_eac_violation_)(int, __int64, __int64); // [rsp+68h] [rbp-20h]
+  void (__fastcall *debug_eac_violation_)(int, __int64, const CHAR *); // [rsp+68h] [rbp-20h]
 
   update_eac_client_ = fn;
   update_eac_client = fn;
   v7 = sub_745860;
-  if ( !eac::is_inited || !(*(**(&eac_module + 1) + 0x18i64))(*(&eac_module + 1), &v4, &v3) )
+  if ( !eac::is_inited || !(*(**(&eac_module + 1) + 0x18LL))(*(&eac_module + 1), &v4, &v3) )
     goto LABEL_5;
   do
     (v7)(v4, v3, code);
-  while ( (*(**(&eac_module + 1) + 0x18i64))(*(&eac_module + 1), &v4, &v3) );
+  while ( (*(**(&eac_module + 1) + 0x18LL))(*(&eac_module + 1), &v4, &v3) );
   update_eac_client_ = update_eac_client;
   if ( update_eac_client )
 LABEL_5:
-    update_eac_client_(code, 0i64, 0i64);
+    update_eac_client_(code, 0LL, 0LL);
 
   // isInGame
   // return *(game + 0xCA) && *(game + 0xCB) && *(animated_loading_job + 0x1E) == 0;
   result = game;
   if ( *(game + 0xCA) )
   {
+    // inGame too
     result = *(game + 0xCB);
     if ( (result & 1) != 0 )
     {
@@ -40,13 +41,13 @@ LABEL_5:
         manager_invoker_2 = fn;
         debug_eac_violation_ = debug_eac_violation;
         if ( !eac::is_inited )
-          return manager_invoker_1(unk_var, 0i64, 0i64);
+          return manager_invoker_1(unk_var, 0LL, 0LL);
 
-        // check_integrity_check_violation?
-        result = (*(**(&eac_module + 1) + 0x28i64))(*(&eac_module + 1), sub_1F49EC0, 0i64, unk_var);
+        // check_integrity_check_violation
+        result = (*(**(&eac_module + 1) + 0x28LL))(*(&eac_module + 1), sub_1F49EC0, 0LL, unk_var);
         manager_invoker_1 = manager_invoker_2;
         if ( manager_invoker_2 )
-          return manager_invoker_1(unk_var, 0i64, 0i64);
+          return manager_invoker_1(unk_var, 0LL, 0LL);
       }
     }
   }
